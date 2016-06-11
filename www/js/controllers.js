abed052c1e62530de78dcd223cf0004face9a3e2
vesaliE <ellangovesali@gmail.com>
2016-06-11 19:07:49 +0800

    new files

---
+++ www/js/controllers.js
@@ -0,0 +1,250 @@
+angular.module('app.controllers', [])
+  
+.controller('yumNUSCtrl', function($scope) {
+
+})
+   
+.controller('chooseCafeCtrl', function($scope) {
+
+})
+   
+.controller('checkFastfoodsCtrl', function($scope) {
+
+})
+   
+.controller('chooseCanteenCtrl', function($scope) {
+
+})
+   
+.controller('bizCanteenCtrl', function($scope) {
+
+})
+   
+.controller('seeLahCtrl', function($scope) {
+
+})
+   
+.controller('suggestionsCtrl', function($scope) {
+
+})
+   
+.controller('fassCanteenCtrl', function($scope) {
+
+})
+   
+.controller('seeLah2Ctrl', function($scope) {
+
+})
+   
+.controller('flavoursUTownCtrl', function($scope) {
+
+})
+   
+.controller('seeLah3Ctrl', function($scope) {
+
+})
+   
+.controller('fOECanteenCtrl', function($scope) {
+
+})
+   
+.controller('seeLah4Ctrl', function($scope) {
+
+})
+   
+.controller('koufuFoodcourtCtrl', function($scope) {
+
+})
+   
+.controller('seeLah5Ctrl', function($scope) {
+
+})
+   
+.controller('scienceCanteenCtrl', function($scope) {
+
+})
+   
+.controller('seeLah6Ctrl', function($scope) {
+
+})
+   
+.controller('yIHFoodJunctionCtrl', function($scope) {
+
+})
+   
+.controller('seeLah7Ctrl', function($scope) {
+
+})
+   
+.controller('macdonaldsFOECtrl', function($scope) {
+
+})
+   
+.controller('seeLah8Ctrl', function($scope) {
+
+})
+   
+.controller('subwayYIHCtrl', function($scope) {
+
+})
+   
+.controller('seeLah9Ctrl', function($scope) {
+
+})
+   
+.controller('subwayFOECtrl', function($scope) {
+
+})
+   
+.controller('seeLah10Ctrl', function($scope) {
+
+})
+   
+.controller('subwayScienceCtrl', function($scope) {
+
+})
+   
+.controller('seeLah11Ctrl', function($scope) {
+
+})
+   
+.controller('subwayUTownCtrl', function($scope) {
+
+})
+   
+.controller('seeLah12Ctrl', function($scope) {
+
+})
+   
+.controller('seeLah13Ctrl', function($scope) {
+
+})
+   
+.controller('alcoveAsianRestaurantBarCtrl', function($scope) {
+
+})
+   
+.controller('aRTFoodHouseCtrl', function($scope) {
+
+})
+   
+.controller('seeLah14Ctrl', function($scope) {
+
+})
+   
+.controller('fortuneVillageCtrl', function($scope) {
+
+})
+   
+.controller('seeLah15Ctrl', function($scope) {
+
+})
+   
+.controller('goodNewsCafeCtrl', function($scope) {
+
+})
+   
+.controller('seeLah16Ctrl', function($scope) {
+
+})
+   
+.controller('humbleOriginsCtrl', function($scope) {
+
+})
+   
+.controller('seeLah17Ctrl', function($scope) {
+
+})
+   
+.controller('hwangSKoreanRestaurantCtrl', function($scope) {
+
+})
+   
+.controller('seeLah18Ctrl', function($scope) {
+
+})
+   
+.controller('playtpusFoodbarCtrl', function($scope) {
+
+})
+   
+.controller('seeLah19Ctrl', function($scope) {
+
+})
+   
+.controller('reedzCafeCtrl', function($scope) {
+
+})
+   
+.controller('seeLah20Ctrl', function($scope) {
+
+})
+   
+.controller('saporeItalianoCtrl', function($scope) {
+
+})
+   
+.controller('seeLah21Ctrl', function($scope) {
+
+})
+   
+.controller('spinelliCtrl', function($scope) {
+
+})
+   
+.controller('seeLah22Ctrl', function($scope) {
+
+})
+   
+.controller('spiceTableByPinesCtrl', function($scope) {
+
+})
+   
+.controller('seeLah23Ctrl', function($scope) {
+
+})
+   
+.controller('starbucksMD11Ctrl', function($scope) {
+
+})
+   
+.controller('seeLah24Ctrl', function($scope) {
+
+})
+   
+.controller('starbucksYIHCtrl', function($scope) {
+
+})
+   
+.controller('seeLah25Ctrl', function($scope) {
+
+})
+   
+.controller('starbucksUTownCtrl', function($scope) {
+
+})
+   
+.controller('seeLah26Ctrl', function($scope) {
+
+})
+   
+.controller('universityClubCtrl', function($scope) {
+
+})
+   
+.controller('seeLah27Ctrl', function($scope) {
+
+})
+   
+.controller('waaCowCtrl', function($scope) {
+
+})
+   
+.controller('seeLah28Ctrl', function($scope) {
+
+})
+   
+.controller('pageCtrl', function($scope) {
+
+})
+ 
