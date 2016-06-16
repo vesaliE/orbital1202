0c066a45b0b5a060266095024872877a01498284
Ellango Vesali <ellangovesali@gmail.com>
2016-06-16 14:01:09 +0800

    Updated js files for geolocation services

---
+++ factory_food.js
@@ -0,0 +1,52 @@
+// This is were all restaurants data sits usedin restaurantList.html. Only for NUS.
+// eventually this will come from API/webservices
+
+angular.module('wmapp.factory_food', ['greatCircles'])
+
+    .factory('foodFactory', function () {
+        "use strict";
+        var factory = {
+            Restaurants : [
+                    //canteens 
+                    {Name: 'Biz Canteen - The Terrace', venueType: 'Canteen', itemid: 'Canteen001', lat:1.2956205, long: 103.7741585, icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'},
+                    {Name: 'FASS Canteen - The Deck', venueType: 'Canteen ',  itemid: 'Canteen002', lat: 1.2949143, long: 103.7717837, icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'},
+                    {Name: 'Flavours @ UTown', venueType: 'Canteen',  itemid: 'Canteen003', lat: 1.3048615, long: 103.7724473, icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'},
+                    {Name: 'FOE Canteen - Techno Edge', venueType: 'Canteen', itemid: 'Canteen004', lat: 1.2983509 , long: 103.7711677, icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'},
+                    {Name: 'Koufu Foodcourt', venueType: 'Canteen ', itemid: 'Canteen005',lat: 1.3038157, long: 103.7739868 , icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'},
+                    {Name: 'Science Canteen', venueType: 'Canteen ',  itemid: 'Canteen006',  lat: 1.2966224, long: 103.7805718, icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'},
+                    {Name: 'Food Junction YIH', venueType: 'Canteen ', itemid: 'Canteen007',lat: 1.2983767, long: 103.7745437, icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'},
+                    //cafes
+                    {Name: 'Alcove Asian Restaurant Bar',  venueType: 'Cafe', itemid: 'Cafe001', lat: 1.3020569, long: 103.7724088, icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'},
+                    {Name: 'A.R.T. Food House ',  venueType: 'Cafe', itemid: 'Cafe002', lat: 48, long: -24, icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'},
+                    {Name: 'Butter My Bun', venueType: 'Cafe', itemid: 'Cafe014', lat: 1.3047341, long: 103.7725725, icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'}, 
+                    {Name: 'Fortune Village',  venueType: 'Cafe', itemid: 'Cafe003', lat:48 , long: -24, icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'},
+                    {Name: 'Good News Cafe',  venueType: 'Cafe', itemid: 'Cafe004', lat: 48.34232, long: -24.4563, icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'},
+                    {Name: 'Humble Origins',  venueType: 'Cafe', itemid: 'Cafe005', lat: 1.2950642, long: 103.7689681,  icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'},
+                    {Name: 'The Hwang\'s Restaurant University Town ',  venueType: 'Cafe', itemid: 'Cafe006', lat: 1.3038157, long: 103.7739868, icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'},
+                    {Name: 'The Royals Bistro Café ',  venueType: 'Cafe', itemid: 'Cafe013', lat: 1.3039084, long: 103.7741073, icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'},
+                    {Name: 'Platypus Food Bar (LT27)',  venueType: 'Cafe', itemid: 'Cafe007', lat: 1.2967775, long: 103.7809592, icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'},
+                    {Name: 'Reedz Café NUS Business School',  venueType: 'Cafe', itemid: 'Cafe008', lat: 1.2925654, long: 103.7719733, icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'},
+                    {Name: 'Sapore Italiano',  venueType: 'Cafe', itemid: 'Cafe009', lat: 1.3041097, long: 103.7740535, icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'},
+                    {Name: 'Spinelli Coffee ',  venueType: 'Cafe', itemid: 'Cafe010', lat: 1.2964077,  long: 103.7805198, icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'},
+                    {Name: 'Spice Table by Pines',  venueType: 'Cafe', itemid: 'Cafe011', lat: 1.3038699, long: 103.7741271, icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'},
+                    {Name: 'Starbucks (MD11)',  venueType: 'Cafe', itemid: 'Cafe012', lat:  1.2941412, long: 103.781285, icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'},
+                    {Name: 'Starbucks @ YIH ',  venueType: 'Cafe', itemid: 'Cafe015', lat: 1.2972787, long: 103.7724656, icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'},
+                    {Name: 'University Club ',  venueType: 'Cafe', itemid: 'Cafe016', lat: 1.3056346, long: 103.772908, icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'},
+                    {Name: 'Waa Cow ',  venueType: 'Cafe', itemid: 'Cafe017', lat: 1.2937922, long:103.7729176, icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'},
+                    //fastfood 
+                    {Name: 'McDonald\'s Restaurant NUS  ',  venueType: 'Fastfood', itemid: 'fastfood001', lat: 1.2984307, long:103.7712874 , icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'},
+                    {Name: 'Subway @ YIH  ',  venueType: 'Fastfood', itemid: 'fastfood002', lat: 1.2980747, long: 103.7742972, icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'},
+                    {Name: 'Subway @ Utown  ',  venueType: 'Fastfood', itemid: 'fastfood003', lat: 1.303689, long: 103.773356, icon: 'img/restaurant_pointer_WMcustom_40x49_v3.png'}
+            ],
+            getRestaurants : function () {
+              return factory.Restaurants;
+            },
+          };
+      return factory;
+    
+    });
+
+
+
+
+
---
+++ routes.js
@@ -0,0 +1,373 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('yumNUS', {
+    url: '/page1',
+    templateUrl: 'templates/yumNUS.html',
+    controller: 'yumNUSCtrl'
+  })
+
+  .state('chooseCafe', {
+    url: '/choose_cafe',
+    templateUrl: 'templates/chooseCafe.html',
+    controller: 'chooseCafeCtrl'
+  })
+
+  .state('checkFastfoods', {
+    url: '/check_fastfood',
+    templateUrl: 'templates/checkFastfoods.html',
+    controller: 'checkFastfoodsCtrl'
+  })
+
+  .state('chooseCanteen', {
+    url: '/choose_canteen',
+    templateUrl: 'templates/chooseCanteen.html',
+    controller: 'chooseCanteenCtrl'
+  })
+
+  .state('bizCanteen', {
+    url: '/biz_canteen',
+    templateUrl: 'templates/bizCanteen.html',
+    controller: 'bizCanteenCtrl'
+  })
+
+  .state('bizCanteen_contribute', {
+    url: '/biz_canteen_contribute',
+    templateUrl: 'templates/bizCanteen_contribute.html',
+    controller: 'bizCanteen_contributeCtrl'
+  })
+
+  .state('temp', {
+    url: '/temp',
+    templateUrl: 'templates/temp.html',
+    controller: 'tempCtrl'
+  })
+
+  .state('seeLah', {
+    url: '/biz_canteen_sl',
+    templateUrl: 'templates/seeLah.html',
+    controller: 'seeLahCtrl'
+  })
+
+  .state('showNearBy', {
+    url: '/show_near_by',
+    templateUrl: 'templates/showNearBy.html',
+    controller: 'restaurantlistController'
+  })
+
+  .state('fassCanteen', {
+    url: '/fass_canteen',
+    templateUrl: 'templates/fassCanteen.html',
+    controller: 'fassCanteenCtrl'
+  })
+
+  .state('seeLah2', {
+    url: '/fass_canteen_sl',
+    templateUrl: 'templates/seeLah2.html',
+    controller: 'seeLah2Ctrl'
+  })
+
+  .state('flavoursUTown', {
+    url: '/flavours',
+    templateUrl: 'templates/flavoursUTown.html',
+    controller: 'flavoursUTownCtrl'
+  })
+
+  .state('seeLah3', {
+    url: '/flavours_sl',
+    templateUrl: 'templates/seeLah3.html',
+    controller: 'seeLah3Ctrl'
+  })
+
+  .state('fOECanteen', {
+    url: '/foe_canteen',
+    templateUrl: 'templates/fOECanteen.html',
+    controller: 'fOECanteenCtrl'
+  })
+
+  .state('seeLah4', {
+    url: '/foe_canteen_sl',
+    templateUrl: 'templates/seeLah4.html',
+    controller: 'seeLah4Ctrl'
+  })
+
+  .state('koufuFoodcourt', {
+    url: '/koufu_foodcourt',
+    templateUrl: 'templates/koufuFoodcourt.html',
+    controller: 'koufuFoodcourtCtrl'
+  })
+
+  .state('seeLah5', {
+    url: '/koufu_foodcourt_sl',
+    templateUrl: 'templates/seeLah5.html',
+    controller: 'seeLah5Ctrl'
+  })
+
+  .state('scienceCanteen', {
+    url: '/science_canteen',
+    templateUrl: 'templates/scienceCanteen.html',
+    controller: 'scienceCanteenCtrl'
+  })
+
+  .state('seeLah6', {
+    url: '/science_canteen_sl',
+    templateUrl: 'templates/seeLah6.html',
+    controller: 'seeLah6Ctrl'
+  })
+
+  .state('yIHFoodJunction', {
+    url: '/yih_food_junction',
+    templateUrl: 'templates/yIHFoodJunction.html',
+    controller: 'yIHFoodJunctionCtrl'
+  })
+
+  .state('seeLah7', {
+    url: '/yih_food_junction_sl',
+    templateUrl: 'templates/seeLah7.html',
+    controller: 'seeLah7Ctrl'
+  })
+
+  .state('macdonaldsFOE', {
+    url: '/macdonalds_foe',
+    templateUrl: 'templates/macdonaldsFOE.html',
+    controller: 'macdonaldsFOECtrl'
+  })
+
+  .state('seeLah8', {
+    url: '/macdonalds_foe_sl',
+    templateUrl: 'templates/seeLah8.html',
+    controller: 'seeLah8Ctrl'
+  })
+
+  .state('subwayYIH', {
+    url: '/subway_yih',
+    templateUrl: 'templates/subwayYIH.html',
+    controller: 'subwayYIHCtrl'
+  })
+
+  .state('seeLah9', {
+    url: '/subway_yih_sl',
+    templateUrl: 'templates/seeLah9.html',
+    controller: 'seeLah9Ctrl'
+  })
+
+  
+
+  .state('subwayUTown', {
+    url: '/subway_utown',
+    templateUrl: 'templates/subwayUTown.html',
+    controller: 'subwayUTownCtrl'
+  })
+
+  .state('seeLah12', {
+    url: '/subway_utown_sl',
+    templateUrl: 'templates/seeLah12.html',
+    controller: 'seeLah12Ctrl'
+  })
+
+  .state('seeLah13', {
+    url: '/alcove_asian_restaurant_bar_sl',
+    templateUrl: 'templates/seeLah13.html',
+    controller: 'seeLah13Ctrl'
+  })
+
+  .state('alcoveAsianRestaurantBar', {
+    url: '/alcove_asian_restaurant_bar',
+    templateUrl: 'templates/alcoveAsianRestaurantBar.html',
+    controller: 'alcoveAsianRestaurantBarCtrl'
+  })
+
+  .state('humbleOrigins', {
+    url: '/humble_origins ',
+    templateUrl: 'templates/humbleOrigins.html',
+    controller: 'humbleOriginsCtrl'
+  })
+
+  .state('seeLah17', {
+    url: '/humble_origins_sl',
+    templateUrl: 'templates/seeLah17.html',
+    controller: 'seeLah17Ctrl'
+  })
+
+  .state('hwangSKoreanRestaurant', {
+    url: '/hwang_korean_restaurant',
+    templateUrl: 'templates/hwangSKoreanRestaurant.html',
+    controller: 'hwangSKoreanRestaurantCtrl'
+  })
+
+  .state('seeLah18', {
+    url: '/hwang_korean_restaurant_sl ',
+    templateUrl: 'templates/seeLah18.html',
+    controller: 'seeLah18Ctrl'
+  })
+
+  .state('playtpusFoodbar', {
+    url: '/platypus_foodbar',
+    templateUrl: 'templates/playtpusFoodbar.html',
+    controller: 'playtpusFoodbarCtrl'
+  })
+
+  .state('seeLah19', {
+    url: '/platypus_foodbar_sl',
+    templateUrl: 'templates/seeLah19.html',
+    controller: 'seeLah19Ctrl'
+  })
+
+  .state('reedzCafe', {
+    url: '/reedz_cafe ',
+    templateUrl: 'templates/reedzCafe.html',
+    controller: 'reedzCafeCtrl'
+  })
+
+  .state('seeLah20', {
+    url: '/reedz_foodbar_sl',
+    templateUrl: 'templates/seeLah20.html',
+    controller: 'seeLah20Ctrl'
+  })
+
+  .state('saporeItaliano', {
+    url: '/sapore_italiano',
+    templateUrl: 'templates/saporeItaliano.html',
+    controller: 'saporeItalianoCtrl'
+  })
+
+  .state('seeLah21', {
+    url: '/sapore_italiano_sl',
+    templateUrl: 'templates/seeLah21.html',
+    controller: 'seeLah21Ctrl'
+  })
+
+  .state('spinelli', {
+    url: '/spinelli',
+    templateUrl: 'templates/spinelli.html',
+    controller: 'spinelliCtrl'
+  })
+
+  .state('seeLah22', {
+    url: '/spinelli_sl',
+    templateUrl: 'templates/seeLah22.html',
+    controller: 'seeLah22Ctrl'
+  })
+
+  .state('spiceTableByPines', {
+    url: '/spice_table_by_pines',
+    templateUrl: 'templates/spiceTableByPines.html',
+    controller: 'spiceTableByPinesCtrl'
+  })
+
+  .state('seeLah23', {
+    url: '/spice_table_by_pines_sl',
+    templateUrl: 'templates/seeLah23.html',
+    controller: 'seeLah23Ctrl'
+  })
+
+  .state('starbucksMD11', {
+    url: '/starbucks_md11',
+    templateUrl: 'templates/starbucksMD11.html',
+    controller: 'starbucksMD11Ctrl'
+  })
+
+  .state('seeLah24', {
+    url: '/starbucks_md11_sl',
+    templateUrl: 'templates/seeLah24.html',
+    controller: 'seeLah24Ctrl'
+  })
+
+  .state('starbucksYIH', {
+    url: '/starbucks_yih',
+    templateUrl: 'templates/starbucksYIH.html',
+    controller: 'starbucksYIHCtrl'
+  })
+
+  .state('seeLah25', {
+    url: '/starbucks_yih_sl',
+    templateUrl: 'templates/seeLah25.html',
+    controller: 'seeLah25Ctrl'
+  })
+
+  .state('starbucksUTown', {
+    url: '/starbucks_utown',
+    templateUrl: 'templates/starbucksUTown.html',
+    controller: 'starbucksUTownCtrl'
+  })
+
+  .state('seeLah26', {
+    url: '/starbucks_utown_sl',
+    templateUrl: 'templates/seeLah26.html',
+    controller: 'seeLah26Ctrl'
+  })
+
+  .state('universityClub', {
+    url: '/university_club',
+    templateUrl: 'templates/universityClub.html',
+    controller: 'universityClubCtrl'
+  })
+
+  .state('seeLah27', {
+    url: '/university_club_sl ',
+    templateUrl: 'templates/seeLah27.html',
+    controller: 'seeLah27Ctrl'
+  })
+
+  .state('waaCow', {
+    url: '/waa_cow ',
+    templateUrl: 'templates/waaCow.html',
+    controller: 'waaCowCtrl'
+  })
+
+  .state('seeLah28', {
+    url: '/waa_cow_sl ',
+    templateUrl: 'templates/seeLah28.html',
+    controller: 'seeLah28Ctrl'
+  })
+
+  .state('page', {
+    url: '/page62',
+    templateUrl: 'templates/page.html',
+    controller: 'pageCtrl'
+  })
+
+ .state('geolocationTest', {
+    url: '/geolocationTest',
+    templateUrl: 'templates/geolocationTest.html',
+    controller: 'GeoCtrl'
+  })
+
+  .state('butterMyBun', {
+    url: '/butter_my_bun',
+    templateUrl: 'templates/butterMyBun.html',
+    controller: 'butterMyBunCtrl'
+  })
+
+  .state('seeLah14', {
+    url: '/butter_my_bun_sl',
+    templateUrl: 'templates/seeLah14.html',
+    controller: 'seeLah14Ctrl'
+  })
+
+  .state('theRoyalsBistroCafe', {
+    url: '/the_royals_bistro_cafe',
+    templateUrl: 'templates/theRoyalsBistroCafe.html',
+    controller: 'theRoyalsBistroCafeCtrl'
+  })
+  
+  .state('seeLah15', {
+    url: '/the_royals_bistro_cafe_sl',
+    templateUrl: 'templates/seeLah15.html',
+    controller: 'seeLah15Ctrl'
+  })
+
+$urlRouterProvider.otherwise('/page1')
+
+  
+
+});
